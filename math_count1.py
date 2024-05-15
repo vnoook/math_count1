67e1ba132252a48e961c3f528d8c24f766e8987e
noook <noook@yandex.ru>
2024-05-15 12:47:09 +0700

    #4

--- math_count1.py
+++ math_count1.py
@@ -1,7 +1,8 @@
 # начальные данные 10-тизначных чисел
 x = 1_000_000_000
+x = 1_111_111_111
 y = 9_999_999_999
-y = 1_040_000_021
+y = 1_111_111_119
 
 # словарь в котором буду сохранять количество количество вариантов
 dict_of_variants = {}
@@ -11,17 +12,26 @@ for num in range(x, y+1):
     print(num_in_str)
 
     q_even_digit = 0
-    q_even_odd = 0
+    q_odd_digit = 0
     for digit in num_in_str:
         # print(digit)
         if digit % 2 == 0:
             q_even_digit += 1
             # print(digit, 'четное')
         else:
-            q_even_odd += 1
+            q_odd_digit += 1
             # print(digit, 'нечетное')
 
-    print(q_even_digit, 'четных,', q_even_odd, 'нечетных')
+    flag_of_key = str(q_even_digit) + str(q_odd_digit)
+    print(flag_of_key)
+
+    # # заполнение словаря отделений
+    # if dict_of_variants.get(val_str[0]) is None:
+    #     dict_of_variants[val_str[0]] = 1
+    # else:
+    #     dict_of_variants[val_str[0]] = dict_departments_fresh[val_str[0]] + 1
+
+    print(q_even_digit, 'четных,', q_odd_digit, 'нечетных')
     print()
 
 print()
