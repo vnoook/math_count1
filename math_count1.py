0ceedcbda3aee8210e4dda815b5d507dd6ced283
noook <noook@yandex.ru>
2024-05-21 16:54:10 +0700

    #8 создание двух подсчётов времени

--- math_count1.py
+++ math_count1.py
@@ -1,13 +1,20 @@
-# начальные данные 10-тизначных чисел
+import time
+import datetime
+
+# начальные данные 10-значных чисел
 x = 1_000_000_000
 # x = 1_111_111_101
-y = 9_999_999_999
-# y = 1_001_111_129
+# y = 9_999_999_999
+y = 1_000_010_129
 # y = 1_112_111_120
 
-# словарь в котором буду сохранять количество количество вариантов
+# вариант 1
+time_start1 = time.time()
+
+# словарь в котором буду сохранять количество вариантов
 dict_of_variants = {}
 
+# подсчёт количества чисел
 for num in range(x, y+1):
     num_in_str = [int(i) for i in str(num)]
     # print(num_in_str)
@@ -23,18 +30,23 @@ for num in range(x, y+1):
             q_odd_digit += 1
             # print(digit, 'нечетное')
 
-    if q_even_digit == 5:
-        flag_of_key = str(q_even_digit) + ' чет - ' + str(q_odd_digit) + ' нечет'
-        # print(flag_of_key)
+    flag_of_key = str(q_even_digit) + ' чет - ' + str(q_odd_digit) + ' нечет'
+    # print(flag_of_key)
 
-        # заполнение словаря отделений
-        if dict_of_variants.get(flag_of_key) is None:
-            dict_of_variants[flag_of_key] = 1
-        else:
-            dict_of_variants[flag_of_key] = dict_of_variants[flag_of_key] + 1
+    # заполнение словаря отделений
+    if dict_of_variants.get(flag_of_key) is None:
+        dict_of_variants[flag_of_key] = 1
+    else:
+        dict_of_variants[flag_of_key] = dict_of_variants[flag_of_key] + 1
 
-        # print(q_even_digit, 'четных,', q_odd_digit, 'нечетных')
-        # print()
+    # print(q_even_digit, 'четных,', q_odd_digit, 'нечетных')
+    # print()
 
 print(dict_of_variants)
+
+time_stop1 = time.time()
+
+time_format = str(datetime.timedelta(seconds = (time_stop1-time_start1)))
 print()
+print('.'*30 + 'закончено за', round(time_stop1-time_start1, 3), 'секунд')
+print('.'*30 + 'закончено за', time_format)
